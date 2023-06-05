c0a06f38882d8e358dded6ed0e33b47c3e198061
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-05 14:34:47 -0300

    Add navbar and modal components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import '../styles/app.css';
 import TodosLogic from './TodosLogic';
+import Navbar from './Navbar';
+import Modal from './Modal';
 
 const TodoApp = () => {
   return (
     <div className="wrapper">
       <div className="todos">
+        <Navbar />
+        <Modal />
         <TodosLogic />
       </div>
     </div>
