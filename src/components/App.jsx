c4a5adbdcc2660c9bbe26525317dcdd483b26d2b
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-05 15:19:04 -0300

    Add nested about components (SPA)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,16 @@ import About from '../routes/About';
 import Login from '../routes/Login';
 import Profile from '../routes/Profile';
 import NotMatch from '../routes/NotMatch';
+import SinglePage from '../routes/SinglePage';
 
 const TodoApp = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="about" element={<About />}>
+          <Route path=":slug" element={<SinglePage />} />
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="profile" element={<Profile />} />
         <Route path="*" element={<NotMatch />} />
