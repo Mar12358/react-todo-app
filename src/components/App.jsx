e3f314b0a1741f350c842376dcd6c6b6868cef1e
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-05 15:01:27 -0300

    Add route paths to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,23 @@
 import '../styles/app.css';
-import TodosLogic from './TodosLogic';
-import Navbar from './Navbar';
-import Modal from './Modal';
+import Layout from './Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../routes/Home';
+import About from '../routes/About';
+import Login from '../routes/Login';
+import Profile from '../routes/Profile';
+import NotMatch from '../routes/NotMatch';
 
 const TodoApp = () => {
   return (
-    <div className="wrapper">
-      <div className="todos">
-        <Navbar />
-        <Modal />
-        <TodosLogic />
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 };
 export default TodoApp;
