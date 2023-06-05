58020561be9bfa9e7bf1e4e54930a0f820db124a
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-05 16:05:44 -0300

    Add ProtectedRout for profile link and hide it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import Login from '../routes/Login';
 import Profile from '../routes/Profile';
 import NotMatch from '../routes/NotMatch';
 import SinglePage from '../routes/SinglePage';
+import ProtectedRoute from '../components/ProtectedRoute';
 
 const TodoApp = () => {
   return (
@@ -17,7 +18,11 @@ const TodoApp = () => {
           <Route path=":slug" element={<SinglePage />} />
         </Route>
         <Route path="login" element={<Login />} />
-        <Route path="profile" element={<Profile />} />
+        <Route path="profile" element={
+          <ProtectedRoute> 
+            <Profile />
+          </ProtectedRoute>
+        } />
         <Route path="*" element={<NotMatch />} />
       </Route>
     </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,6 +35,14 @@ const Navbar = () => {
                   <NavLink to={link.path}>{link.text}</NavLink>
                 </li>
               )
+            ) : link.path === 'profile' ? (
+              user && (
+                <li>
+                  <NavLink to={link.path}>
+                    {link.text}
+                  </NavLink>
+                </li>
+              )
             ) : (
               <li>
                 <NavLink to={link.path}>{link.text}</NavLink>
