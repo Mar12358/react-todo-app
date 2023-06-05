eab0075eb5e296a7cbe22b6189bdf40dc27dc4ea
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-05 12:49:33 -0300

    Add code to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
+import TodosLogic from './components/TodosLogic';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TodosLogic />
       </header>
     </div>
   );
