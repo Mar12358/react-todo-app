2fdaade183a9af589b90770245aafacf80cd8e74
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-06 13:00:44 -0300

    Fix Linters

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import styles from '../styles/Login.module.css';
 import { useAuthContext } from '../context/AuthContext';
 
 const Login = () => {
